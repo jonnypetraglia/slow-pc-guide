ca5e4f7c8d835d102becffcf0180c12b9a042ba2
Jon Petraglia <notbryant@gmail.com>
2015-08-02 01:34:27 -0600

    Added a lot of stuff and fixed generation, period
      - Adjusted permalinks for gh_pages to point cross-chapter
      - Made HTML ebook include CSS in it
      - Filled out comments
      - Beautifying output HTML (optional)

--- build.js
+++ build.js
@@ -11,54 +11,83 @@ var fs = require('fs')
 var mkdirp = require('mkdirp')
   , Promise = (global.Promise || require('promiscuous'))
   , ebrew = require('ebrew')
-  , markdown = require('markdown-it')('commonmark')
-    .use(require('markdown-it-anchor'), {permalink: true})
+  , markdown_it = require('markdown-it')
   , wkhtmltopdf = require('wkhtmltopdf')
   , mdtoc = require('markdown-toc')
+  , beautify = function(x) { return x};
+try {
+  // (Optional) This module will make exported HTML easy on the eyes.
+  beautify = require('js-beautify').html
+  beautify()
+} catch(e) {}
+
+// Styles available for HTML exporting
+var styles = {
+  "github":           "github-markdown-css/github-markdown.css",
+  "avenir-white":     "markdown-css-themes/avenir-white.css",
+  "foghorn":          "markdown-css-themes/foghorn.css",
+  "swiss":            "markdown-css-themes/swiss.css",
+}, chosenStyle = 'github', chosenStyleContent = null;
 
 
 // Meta info about the book
 var meta = require('./meta.json'),
     websiteDir = path.join(__dirname, "dist"),
     ebookDir = path.join(__dirname, "dist", "ebook")
+// Useful vars
 var pagebreak = "\n\n******\n\n";
+var TocHash = {};   // ToC entry is key, value is file it is in (hardware_failure -> solutions.md)
 
-
-
-// 0. Create output directories
+// 0. Create output directories, read CSS file
 new Promise(function(resolve, reject) {
-  mkdirp(ebookDir, vow(resolve, reject))
+  return mkdirp(ebookDir, vow(resolve, reject));
 })
-// 1. Read ALL the chapters!
+.then(function() {
+  return new Promise(function(resolve, reject) {
+    var stylePath = path.join(websiteDir, "styles", styles[chosenStyle]);
+    fs.readFile(stylePath, {encoding: 'utf-8'}, vow(resolve, reject));
+  }).then(function(data) {
+    chosenStyleContent = data;
+  });
+})
+// 1. Read ALL the chapters! Also generate TocHash at the same time
 .then(function() {
   return Promise.all(meta.contents.map(function(filename) {
     return new Promise(function(resolve, reject) {
-      fs.readFile(filename, {encoding: 'utf8'}, vow(resolve, reject));
+      fs.readFile(filename, {encoding: 'utf8'}, function(err, data) {
+        if(err) return reject(err);
+        var tocData = mdtoc(data);
+        for(var x in tocData.json) {
+          var hash = mdtoc.slugify(tocData.json[x].content)
+          if(TocHash[hash]) return reject("#"+hash+" used multiple times")
+          TocHash[hash] = filename;
+        }
+        resolve({toc: tocData, md: data, filename: filename});
+      });
     })
   }))
 })
 // 3. Create the Github Pages versions
-.then(function(fileContentsArray) {
-  return Promise.all(fileContentsArray.map(function(fileContents, index) {
+.then(function(fileDataArray) {
+  return Promise.all(fileDataArray.map(function(fileData, index) {
     return new Promise(function(resolve, reject) {
-      var filename = meta.contents[index];
-      if(/^readme/i.test(filename)) filename = "index.md";
+      var filename = readme2index(fileData.filename, ".html");
       fs.writeFile(
-        path.join(websiteDir, filename.replace(/\.[^\.]+$/, '.html')),
-        wrapHTML(insertTOC(fileContents, false), filename),
+        path.join(websiteDir, filename),
+        wrapHTML(fileData.md, fileData.toc, filename),
         vow(resolve, reject)
       );
     })
   })).then(function() {
-    return fileContentsArray;
+    return fileDataArray.map(function(file) {
+      return file.md;
+    }).join(pagebreak);
   })
 })
 // 3. Create the markdown and generate each filetype
-.then(function(fileContentsArary) {
-  var md = insertTOC(fileContentsArary.join(pagebreak), true);
-  
+.then(function(md) {
   return Promise.all(Object.keys(generate).map(function(filetype) {
-    generate[filetype](md, path.join(ebookDir, meta.title+"."+filetype));
+    return generate[filetype](md, path.join(ebookDir, meta.title+"."+filetype));
   }));
 })
 .then(function() {
@@ -95,64 +124,94 @@ var generate = {
       ebrew.generate("meta.json", filename, vow(resolve, reject));
     })
   }
-}
-
-// Styles available for HTML exporting
-var styles = {
-  "github":           "github-markdown-css/github-markdown.css",
-  "avenir-white":     "markdown-css-themes/avenir-white.css",
-  "foghorn":          "markdown-css-themes/foghorn.css",
-  "swiss":            "markdown-css-themes/swiss.css",
-}
+};
 
 
-function insertTOC(md, firsth1) {
-  var toc = "## Table of Contents ##\n\n" + mdtoc(md, {firsth1: firsth1}).content + pagebreak
 
+// Does several things; takes in the markdown and spits out the fully-rendered ToC'ed HTML
+function wrapHTML(md, tocJson, originalFilename) {
+  // 1. Insert the ToC into the markdown
+  var toc = "## Table of Contents ##\n\n" + (tocJson ? tocJson.content : mdtoc(md).content) + pagebreak;
   var delim = "\n##"
   var ind = md.indexOf(delim);
-  if(ind===0)
-    return md
-  return md.slice(0, ind) + toc + md.slice(ind);
-}
+  if(ind !== 0)
+    md = md.slice(0, ind) + toc + md.slice(ind);
 
-function wrapHTML(md, filename) {
+  // 2. Renders the markdown into HTML (including adjusting permalinks)
   var env = {}
-  md = markdown.use(require('markdown-it-title')).render(md, env);
-  var html = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>\n"
-            + env.title + " - " + meta.title
-            + "\n</title>\n";
+  var mdit = markdown_it('commonmark')
+      .use(require('markdown-it-anchor'), {permalink: true})
+      .use(require('markdown-it-title'));
+  if(tocJson)
+    mdit.use(adjustPermalinks_plugin);
+  inputHTML = mdit.render(md, env);
 
-  html = html+"<style>\n"
+  // 3. Wraps the content inside the <body> of a valid HTML page with styles and such
+  var html = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>\n"
+        + env.title + " - " + meta.title
+        + "\n</title>\n"
+        +"<style>\n"
         + "html, body {width:100%;height:100%;margin:0;}\n"
         + ".markdown-body {min-width: 200px;max-width: 790px;margin: 0 auto;padding: 30px;}\n"
         + "#" + mdtoc.slugify(env.title) + " {font-size: 4em}\n"
         + "#table-of-contents + ul {list-style: circle;}\n"
-        + "</style>\n</head>\n<body>"
-  if(filename) {
-    html = html+"<link rel='stylesheet' href='styles/" + styles['github'] + "'>\n"
+        + ":target {background: yellow;}\n"
+        + "</style>\n";
+  if(!originalFilename) {
+    // For a 1-page file, insert the CSS file contents
+    html = html+"<style>\n"+chosenStyleContent+"</style>\n"
+        + "</head>\n<body>\n";
+  } else {
+    // For an individual file (gh_pages), insert links to the CSS and a nav linking to other chapters
+    html = html+"<link rel='stylesheet' href='styles/" + styles[chosenStyle] + "'>\n"
           + "<style>\n"
           + ".site-nav {border-bottom: 1px solid #5A5A5A;line-height: 1.6;font-family: 'Helvetica Neue','Helvetica','Open Sans',sans-serif;font-size: 1.2em;}\n"
           + ".site-nav ul {padding: 0px 20px;display: flex;justify-content: space-between;list-style: outside none none;max-width: 955px;margin: 0px auto;}\n"
           + ".site-nav a {color: #6A6A6A;text-decoration:none;font-weight:400;}\n"
-          + ".site-nav a[href='" + filename.replace(/\.[^\.]+$/, '') + ".html']{color:black;}\n"
+          + ".site-nav a[href='" + readme2index(originalFilename, ".html") + "']{color:black;}\n"
           + "@media screen and (max-width: 800px) {.site-nav ul {flex-direction: column;text-align: center;}}\n"
-          + "</style>\n";
-  }
-  if(filename) {
-    html = html+"<nav class='site-nav'>\n<ul>\n";
-    meta.contents.forEach(function(filename) {
-      var title = filename.replace(/\.[^\.]+$/, '').replace('_', ' ');
-      html = html+"<li><a href='"
-            + (/^readme/.test(filename) ? 'index.html' : filename.replace(/\.[^\.]+$/, '.html'))
-            + "'>" + title + "</a></li>\n"
-    })
-    html = html+"</ul>\n</nav>\n";
+          + "</style>\n"
+          + "</head>\n<body>\n"
+          + "<nav class='site-nav'>\n<ul>\n"
+          + meta.contents.map(function(filename) {
+            var title = replaceExt(filename).replace(/_/g, " ").toLowerCase();
+            return "<li><a href='"
+                  + readme2index(filename, ".html")
+                  + "'>" + title + "</a></li>\n";
+          }).join("\n")
+          + "</ul>\n</nav>\n";
   }
   html = html+"<article class='markdown-body'>\n"
-        + md
-        + "\n</article>\n</body>\n</html>"
-  return html;
+        + inputHTML
+        + "\n</article>\n</body>\n</html>";
+  return beautify(html);
+}
+
+// A plugin for markdown-it that adjust permalinks to specify their chapter based on TocHash
+//    Example: '#ccleaner' => 'solutions.html#ccleaner'
+function adjustPermalinks_plugin(md) {
+  var oldLinkOpenOverride = md.renderer.rules.link_open;
+  md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+    var hrefIndex = tokens[idx].attrIndex('href');
+    if(hrefIndex >= 0 && tokens[idx].attrs[hrefIndex][1].indexOf("#") == 0) {
+      var hrefTarget = tokens[idx].attrs[hrefIndex][1].substr(1);
+      var filename = TocHash[hrefTarget];
+      if(filename)
+        tokens[idx].attrs[hrefIndex][1] = readme2index(filename) + "#" + hrefTarget;
+    }
+    if (oldLinkOpenOverride)
+      return oldLinkOpenOverride.apply(self, arguments);
+    else
+      return self.renderToken.apply(self, arguments);
+  };
+}
+
+function replaceExt(str, ext) {
+  return str.replace(/\.[^\.]+$/, ext || '');
+}
+
+function readme2index(filename, ext) {
+  return /^readme/i.test(filename) ? 'index'+ext : replaceExt(filename, ext);
 }
 
 function vow(res, rej) {
