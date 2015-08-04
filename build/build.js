39382a814e75c75bce1f8c15c5bbd61497ced455
Jon Petraglia <notbryant@gmail.com>
2015-08-03 20:55:45 -0600

    Enhanced the build method
    Added comments.
    Monkey-patched path.dirname to make ebrew happy.
    Added template for HTML file.
    Enhanced the permalink in....ways. I'm hungry.
    Added npm scripts.

--- build/build.js
+++ build/build.js
@@ -6,16 +6,12 @@
  *   - md (one file)
  *   - html (one file)
 */
+
+//////////////////// Imports ////////////////////
+
+
 var fs = require('fs')
   , path = require('path');
-
-// ebrew requires that the markdown files be in the same folder as the JSON file.
-// So let's monkeypatch the call to return the working directory
-var oldDirname = path.dirname;
-path.dirname = function(arg) {
-  return arg==__dirname + "/meta.json" ? process.cwd() : oldDirname(arg);
-};
-
 var mkdirp = require('mkdirp')
   , Promise = (global.Promise || require('promiscuous'))
   , ebrew = require('ebrew')
@@ -29,37 +25,57 @@ try {
   beautify()
 } catch(e) {}
 
+  // ebrew requires that the markdown files be in the same folder as the JSON file.
+  // So let's monkeypatch the call to return the working directory
+var oldDirname = path.dirname;
+path.dirname = function(arg) {
+  return arg==__dirname + "/meta.json" ? process.cwd() : oldDirname(arg);
+};
+
+
+//////////////////// Setup the environment ////////////////////
+
+
 process.chdir(__dirname + "/..")
-
-// Styles available for HTML exporting
-var styles = {
-  "github":           "github-markdown-css/github-markdown.css",
-  "avenir-white":     "markdown-css-themes/avenir-white.css",
-  "foghorn":          "markdown-css-themes/foghorn.css",
-  "swiss":            "markdown-css-themes/swiss.css",
-}, chosenStyle = 'github', chosenStyleContent = null;
-
-
 
 // Meta info about the book
 var meta = require(__dirname + "/meta.json"),
     websiteDir = path.resolve(process.cwd(), "dist"),
-    ebookDir = path.resolve(process.cwd(), "dist", "ebook")
+    ebookDir = path.resolve(process.cwd(), "dist", "ebook"),
 // Useful vars
-var pagebreak = "\n\n******\n\n";
-var TocHash = {};   // ToC entry is key, value is file it is in (hardware_failure -> solutions.md)
+    pagebreak = "\n\n******\n\n",
+    TocHash = {},   // ToC entry is key, value is file it is in (hardware_failure -> solutions.md)
+// Styles available for HTML exporting
+  styles = {
+  "github":           "github-markdown-css/github-markdown.css",
+  "avenir-white":     "markdown-css-themes/avenir-white.css",
+  "foghorn":          "markdown-css-themes/foghorn.css",
+  "swiss":            "markdown-css-themes/swiss.css",
+  //https://github.com/markdowncss/air
+}, chosenStyle = "github" 
+, fileContents = {
+  style:    path.join(websiteDir, "styles", styles[chosenStyle]),
+  template: path.join(__dirname, "templ.html"),
+  //image:    path.join(__dirname, "noun_22554_cc.png"),
+  //favicon:  path.join(__dirname, "favicon.png"),
+};
 
-// 0. Create output directories, read CSS file
+
+//////////////////// Start the promise chain ////////////////////
+
+
+// 0. Create output directories, read misc files
 new Promise(function(resolve, reject) {
   return mkdirp(ebookDir, vow(resolve, reject));
 })
 .then(function() {
-  return new Promise(function(resolve, reject) {
-    var stylePath = path.join(websiteDir, "styles", styles[chosenStyle]);
-    fs.readFile(stylePath, {encoding: 'utf-8'}, vow(resolve, reject));
-  }).then(function(data) {
-    chosenStyleContent = data;
-  });
+  return Promise.all(Object.keys(fileContents).map(function(key) {
+    return new Promise(function(resolve, reject) {
+      fs.readFile(fileContents[key], {encoding: 'utf-8'}, vow(resolve, reject));
+    }).then(function(data) {
+      fileContents[key] = data;
+    });
+  }));
 })
 // 1. Read ALL the chapters! Also generate TocHash at the same time
 .then(function() {
@@ -69,8 +85,8 @@ new Promise(function(resolve, reject) {
         if(err) return reject(err);
         var tocData = mdtoc(data);
         for(var x in tocData.json) {
-          var hash = mdtoc.slugify(tocData.json[x].content)
-          if(TocHash[hash]) return reject("#"+hash+" used multiple times")
+          var hash = mdtoc.slugify(tocData.json[x].content);
+          if(TocHash[hash]) return reject("#"+hash+" used multiple times");
           TocHash[hash] = filename;
         }
         resolve({toc: tocData, md: data, filename: filename});
@@ -109,8 +125,8 @@ new Promise(function(resolve, reject) {
   throw err
 })
 
+//////////////////// Filetype generation ////////////////////
 
-// Functions for generating the various file formats
 var generate = {
   md: function(md, filename) {
     return new Promise(function(resolve, reject) {
@@ -122,7 +138,6 @@ var generate = {
       fs.writeFile(filename, wrapHTML(md), vow(resolve, reject))
     })
   },
-  /*
   pdf: function(md, filename) {
     return new Promise(function(resolve, reject) {
       wkhtmltopdf(wrapHTML(md), { output: filename }, function (code, signal) {
@@ -131,37 +146,14 @@ var generate = {
       });
     });
   },
-  */
   epub: function(md, filename) {
     return new Promise(function(resolve, reject) {
-      console.log(process.cwd())
       ebrew.generate(__dirname + "/meta.json", filename, vow(resolve, reject));
     })
   }
 };
 
-
-// Builds on top of markdown-it-anchor by:
-//   1. Adding a 'title' attribute
-//   2. Making an Anchor yield the heading text when bookmarked
-//      - done by inserting the text into an invisible <span> inside the <a>
-var renderPermalink = function(slug, opts, tokens, idx) {
-  require('markdown-it-anchor').defaults.renderPermalink(slug, opts, tokens, idx);
-  var children = tokens[idx+1].children;
-  var a=0;
-  while(a<children.length && children[a].type!="link_open")
-    a+=1;
-  children[a].attrs.push(['title', 'Permalink']);
-  
-  children[a+1].content = meta.title
-  if(children[0].content.toLowerCase() != meta.title.toLowerCase())
-    children[a+1].content = children[0].content + " - " + children[a+1].content
-  
-  var span = {type: "span_open", tag:"span"}
-  for(var k in children[a+1])
-    if(!span[k]) span[k] = children[a+1][k];
-  children.splice(a+1, 0, span);
-};
+//////////////////// Auxiliary functions ////////////////////
 
 
 // Does several things; takes in the markdown and spits out the fully-rendered ToC'ed HTML
@@ -181,50 +173,55 @@ function wrapHTML(md, tocJson, originalFilename) {
       .use(require('markdown-it-title'));
   if(tocJson)
     mdit.use(adjustPermalinks_plugin);
-  inputHTML = mdit.render(md, env);
+  var pagehtml = mdit.render(md, env);
 
-  // 3. Wraps the content inside the <body> of a valid HTML page with styles and such
-  var html = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>\n"
-        + env.title + " - " + meta.title
-        + "\n</title>\n"
-        +"<style>\n"
-        + "html, body {width:100%;height:100%;margin:0;}\n"
-        + ".markdown-body {min-width: 200px;max-width: 790px;margin: 0 auto;padding: 30px;}\n"
-        + "#" + mdtoc.slugify(env.title) + " {font-size: 4em}\n"
-        + "#table-of-contents + ul {list-style: circle;}\n"
-        + ":target {background: yellow;}\n"
-        + ".header-anchor span {display: none;}\n"
-        + ".header-anchor:before {content: '#';}"
-        + "</style>\n";
-  if(!originalFilename) {
-    // For a 1-page file, insert the CSS file contents
-    html = html+"<style>\n"+chosenStyleContent+"</style>\n"
-        + "</head>\n<body>\n";
-  } else {
-    // For an individual file (gh_pages), insert links to the CSS and a nav linking to other chapters
-    html = html+"<link rel='stylesheet' href='styles/" + styles[chosenStyle] + "'>\n"
-          + "<style>\n"
-          + ".site-nav {border-bottom: 1px solid #5A5A5A;line-height: 1.6;font-family: 'Helvetica Neue','Helvetica','Open Sans',sans-serif;font-size: 1.2em;}\n"
-          + ".site-nav ul {padding: 0px 20px;display: flex;justify-content: space-between;list-style: outside none none;max-width: 955px;margin: 0px auto;}\n"
-          + ".site-nav a {color: #6A6A6A;text-decoration:none;font-weight:400;}\n"
-          + ".site-nav a[href='" + readme2index(originalFilename, ".html") + "']{color:black;}\n"
-          + "@media screen and (max-width: 800px) {.site-nav ul {flex-direction: column;text-align: center;}}\n"
-          + "</style>\n"
-          + "</head>\n<body>\n"
-          + "<nav class='site-nav'>\n<ul>\n"
-          + meta.contents.map(function(filename) {
-            var title = readme2other(filename, "about").replace(/_/g, " ").toLowerCase();
-            return "<li><a href='"
-                  + readme2index(filename, ".html")
-                  + "'>" + title + "</a></li>\n";
-          }).join("\n")
-          + "</ul>\n</nav>\n";
-  }
-  html = html+"<article class='markdown-body'>\n"
-        + inputHTML
-        + "\n</article>\n</body>\n</html>";
-  return beautify(html);
+  var opts = {
+    title: env.title + " - " + meta.title,
+    titleslug: mdtoc.slugify(env.title),
+    cssurl: "styles/" + styles[chosenStyle],
+    pagehref: readme2index(originalFilename || '', ".html"),
+    sitenav: meta.contents.map(function(filename) {
+      var title = readme2other(filename, "about").replace(/_/g, " ").toLowerCase();
+      return "<li><a href='"
+            + readme2index(filename, ".html")
+            + "'>" + title + "</a></li>";
+    }).join("\n"),
+    pagehtml: pagehtml
+  };
+
+  return beautify(template(fileContents.template, opts));
 }
+
+// Builds on top of markdown-it-anchor by:
+//   1. Adding a 'title' attribute
+//   2. Making an Anchor yield the heading text when bookmarked
+//      - done by inserting the text into an invisible <span> inside the <a>
+var renderPermalink = function(slug, opts, tokens, idx) {
+  require('markdown-it-anchor').defaults.renderPermalink(slug, opts, tokens, idx);
+
+  // Find the <a>
+  var children = tokens[idx+1].children;
+  var a=0;
+  while(a<children.length && children[a].type!="link_open")
+    a+=1;
+
+  // Add 'title'
+  children[a].attrs.push(['title', 'Permalink']);
+  
+  // Alter the text of the <a>
+  children[a+1].content = meta.title
+  if(children[0].content.toLowerCase() != meta.title.toLowerCase())
+    children[a+1].content = children[0].content + " - " + children[a+1].content
+  
+  // Wrap the text in a span
+  var tags = ["span_open", "span_close"];
+  tags.forEach(function(type, index) {
+    var span = {type: "span_open", tag:"span"}
+    for(var k in children[a+1])
+      if(!span[k]) span[k] = children[a+1][k];
+    children.splice(a+1+index*2, 0, span);
+  });
+};
 
 // A plugin for markdown-it that adjust permalinks to specify their chapter based on TocHash
 //    Example: '#ccleaner' => 'solutions.html#ccleaner'
@@ -245,20 +242,31 @@ function adjustPermalinks_plugin(md) {
   };
 }
 
+
+// A stupid simple 'template' function
+function template(str, vars) {
+  Object.keys(vars).forEach(function(key) {
+    str = str.replace(new RegExp("{{"+key+"}}","g"), vars[key]);
+  });
+  return str;
+};
+
+// Replaces a file extension with whatever is specified (note: must include the dot)
 function replaceExt(str, ext) {
   return str.replace(/\.[^\.]+$/, ext || '');
 }
 
+// Converts 'readme.ext' to 'index.ext' (or some 'other' string)
 function readme2index(filename, ext) { return readme2other(filename, "index", ext); }
 function readme2other(filename, other, ext) {
   return /^readme/i.test(filename) ? other+(ext||"") : replaceExt(filename, ext);
 }
 
-function toTitleCase(str)
-{
+function toTitleCase(str) {
   return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+// A lazy way to deal with callbacks in a promise environment
 function vow(res, rej) {
   return function(err, val) {
     if(err) rej(err);
